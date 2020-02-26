4dd577eeff164d5909cbac0b2daaaf92d427bb36
Miroslav Bartoš <bartos.m@hanaboso.com>
2020-02-26 09:02:55 +0100

    Create redux actions

---
+++ lib/redux/actions.dart
@@ -0,0 +1,13 @@
+import 'package:shop_list_redux/model/cart_item.dart';
+
+class AddCartItem {
+  final CartItem cartItem;
+
+  AddCartItem({this.cartItem});
+}
+
+class ToggleItemState {
+  final CartItem cartItem;
+
+  ToggleItemState({this.cartItem});
+}
