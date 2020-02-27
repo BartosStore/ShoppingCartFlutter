c0335dd9a51b9a0aa549ec8b4bf58d8d88447c5e
Miroslav Bartoš <bartos.m@hanaboso.com>
2020-02-27 14:56:46 +0100

    Fix item name

--- lib/api/api_client.dart
+++ lib/api/api_client.dart
@@ -3,6 +3,6 @@ import 'package:shop_list_redux/model/cart_item.dart';
 class ApiClient {
   Future<CartItem> fetchItem() async {
     return new Future.delayed(
-        Duration(seconds: 1), () => CartItem(name: "Fetch", checked: false));
+        Duration(seconds: 1), () => CartItem(name: "Ham", checked: false));
   }
 }
