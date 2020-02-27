4a2120ac6ce4e20d9de5e1af0bf579ec9e33b4dc
Miroslav Bartoš <bartos.m@hanaboso.com>
2020-02-27 11:57:59 +0100

    Show items count

--- lib/shopping_cart.dart
+++ lib/shopping_cart.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 
 import 'package:shop_list_redux/add_item/add_item_dialog.dart';
 
 import 'package:shop_list_redux/list/shopping_list.dart';
+import 'package:shop_list_redux/model/cart_item.dart';
+import 'package:shop_list_redux/redux/app_state.dart';
 import 'package:shop_list_redux/remove_items/remove_items_dialog.dart';
 
 class ShoppingCart extends StatelessWidget {
@@ -22,13 +25,31 @@ class ShoppingCart extends StatelessWidget {
     );
   }
 
+  _renderDescription() {
+    return StoreConnector<AppState, List<CartItem>>(
+      converter: (store) => store.state.cartItems,
+      builder: (context, items) => Container(
+        alignment: Alignment.topLeft,
+        padding: EdgeInsets.all(8.0),
+        child: Text('${items.length} items in your cart'),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('ShoppingCart'),
       ),
-      body: ShoppingList(),
+      body: Column(
+        children: <Widget>[
+          _renderDescription(),
+          Flexible(
+            child: ShoppingList(),
+          ),
+        ],
+      ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       floatingActionButton: Padding(
         padding: EdgeInsets.all(8.0),
