53e730f33ca7accd1b760f73bd0aa0989234567d
Miroslav Bartoš <bartos.m@hanaboso.com>
2020-02-27 14:54:30 +0100

    Create async call via redux thunk

--- lib/list/shopping_list.dart
+++ lib/list/shopping_list.dart
@@ -4,12 +4,14 @@ import 'package:flutter_redux/flutter_redux.dart';
 
 import 'package:shop_list_redux/list/shopping_list_item.dart';
 import 'package:shop_list_redux/model/cart_item.dart';
+import 'package:shop_list_redux/redux/actions.dart';
 import 'package:shop_list_redux/redux/app_state.dart';
 
 class ShoppingList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new StoreConnector<AppState, List<CartItem>>(
+      onInit: (store) => store.dispatch(fetchItemAction()),
       converter: (store) => store.state.cartItems,
       builder: (context, list) {
         return new ListView.builder(
--- lib/redux/actions.dart
+++ lib/redux/actions.dart
@@ -1,4 +1,8 @@
+import 'package:redux/redux.dart';
+import 'package:redux_thunk/redux_thunk.dart';
+import 'package:shop_list_redux/api/api_client.dart';
 import 'package:shop_list_redux/model/cart_item.dart';
+import 'package:shop_list_redux/redux/app_state.dart';
 
 class AddCartItemAction {
   AddCartItemAction({this.item});
@@ -20,4 +24,19 @@ class ToggleItemStateAction {
   ToggleItemStateAction({this.item});
 
   final CartItem item;
+}
+
+/// == Middleware action ==
+/// Every action goes throw thunkMiddleware
+/// but only ThunkAction is resolved there
+ThunkAction<AppState> fetchItemAction() {
+  return (Store<AppState> store) async {
+    ApiClient().fetchItem().then(
+          (item) => store.dispatch(
+            AddCartItemAction(
+              item: CartItem(name: item.name, checked: item.checked),
+            ),
+          ),
+        );
+  };
 }
